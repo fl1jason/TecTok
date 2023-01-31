edfce6494fae49464c95ff1df8db5d07fc0dedf1
fl1jason <jason@fl1digital.com>
2023-01-31 22:26:14 +0000

    changed blog routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const blogRoutes = require('./blogRoutes');
+const blogRoutes = require('./notblogRoutes');
 const commentRoutes = require('./commentRoutes');
 
 router.use('/users', userRoutes);
