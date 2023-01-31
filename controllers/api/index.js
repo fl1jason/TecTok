136f3dd30c8a4485aa88025d006d32a6b4ef0dc9
Raxopagne <OskarZaorski1@gmail.com>
2023-01-31 22:03:56 +0000

    uppercase b

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const BlogRoutes = require('./blogRoutes');
+const blogRoutes = require('./blogRoutes');
 const commentRoutes = require('./commentRoutes');
 
 router.use('/users', userRoutes);
-router.use('/blog', BlogRoutes);
+router.use('/blog', blogRoutes);
 router.use('/comments', commentRoutes);
 
 module.exports = router;
